bc0e5d1e4e48c056dec476a8db77e74c8eb0dac5
orley12 <oosolarin@gmail.com>
2024-07-08 04:00:49 +0100

    add logger protocol add file logger add logger error

---
+++ Sources/Logger/FileLogger.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Oluwatosin Solarin on 08/07/2024.
+//
+
+import Foundation
--- Sources/Logger/Logger.swift
+++ Sources/Logger/Logger.swift
@@ -1,2 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public protocol Logger {
+    func log(error: String) throws -> Void
+}
